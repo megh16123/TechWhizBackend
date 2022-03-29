9049f32487cfbee9e6759299870c1c8cb5d3fcab
HardikBhanot <hardikbhanot123@gmail.com>
2022-03-29 16:15:12 +0530

    Data File

--- routes/hyperionRouter.js
+++ routes/hyperionRouter.js
@@ -1,7 +1,7 @@
 const express  =   require('express');
 const router   =   express.Router();
 const {notices,achievements} = require('../assets/dataGetter')
-
+const data = require('../assets/data.json');
 
 
 router.get('/',(req,res)=>{
@@ -39,20 +39,13 @@ router.get('/principal',(req,res)=>{
 });
 });
 router.get('/:socName',(req,res)=>{
-    const socdata = {
-        subHead:"The Hip Hop Music Society",
-        pBold:"Rapbeats was the first ever Hip Hop Music society formed in Delhi University.",
-        paragraphs:["This is the society which follows hip hop from the soul.Rapbeats represent three vital elements of Hip hop. Rapbeats-The Hip hop Music Society of PGDAV College is a group of talented beatboxers, rappers and music producers",
-        "The artists of Rapbeats performs in various competition across India and had won competitions at the Biggest Asia Cultural Fest Mood Indigo organized by IIT Bombay 4 times across two mazor Competitions ,i.e. Mood Indigo Got Talent and MI-NEM ( The Rap Battle) apart form winning rap battles,Dj Wars and Talent Hunts across 25+ colleges across Delhi.",
-        "Music Producers from our society have their Music launched from Speed Records and also produced music for the aftermovies of various college festivals"
-    ],
-    links:[{username:"/rapbeats7",link:"https://www.instagram.com/rapbeats7/",image:"logos/InstagramLogo.png"},{username:"/rapbeatspgdav",link:"https://www.facebook.com/rapbeatspgdav/",image:"logos/FacebookLogo.png"}],
-    galleryImgs:[`${req.params.socName}/gallery/Gallery1.jpg`,`${req.params.socName}/gallery/Gallery2.jpg`,`${req.params.socName}/gallery/Gallery3.jpg`]
-    }
+    const socdata = data.filter((dat)=>{
+        return dat.slug===req.params.socName;
+    });
     res.render('socView',{
         layout:"navnSide",
         name:req.params.socName,
-        data:socdata
+        data:socdata[0]
 });
 })
 
